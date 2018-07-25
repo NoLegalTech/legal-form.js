f849bdd47feab124f941908a274478bd47ad6a5e
Pepe Doval <pepellou@gmail.com>
2018-07-25 20:22:59 +0200

    DialogSpec

---
+++ spec/DialogSpec.js
@@ -0,0 +1,19 @@
+describe("Dialog", function() {
+
+    describe("constructor", function() {
+
+        xit("something", function() {
+            var theDialog = new Dialog({
+                    info: [],
+                    dialogTitle: '',
+                    tableTitle: '',
+                    submitText: ''
+                }, document.body
+            );
+
+            expect(theDialog.dialog.innerHTML).toEqual('s');
+        });
+
+    });
+
+});
