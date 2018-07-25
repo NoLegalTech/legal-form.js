80ce36bb7d57c29505882426c41b49c7b6199b46
Pepe Doval <pepellou@gmail.com>
2018-07-25 10:18:07 +0200

    Can get form of a button

--- spec/LegalFormSpec.js
+++ spec/LegalFormSpec.js
@@ -1,8 +1,16 @@
 describe("Utils", function() {
 
-  it("getRandomId() starts with legal-form-dialog-", function() {
-    var expectedStart = 'legal-form-dialog-';
-    expect(Utils.getRandomId().substring(0, expectedStart.length)).toEqual(expectedStart);
-  });
+    it("getRandomId() starts with legal-form-dialog-", function() {
+        var expectedStart = 'legal-form-dialog-';
+        expect(Utils.getRandomId().substring(0, expectedStart.length)).toEqual(expectedStart);
+    });
+
+    it("getFormOfButton() returns form of a button", function() {
+        var theForm = document.createElement('form');
+        var theButton = document.createElement('button');
+        theForm.appendChild(theButton);
+
+        expect(getFormOfButton(theButton)).toEqual(theForm);
+    });
 
 });
